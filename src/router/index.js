4264f8efd5107e423faa050c6353f19f45fbd2b5
PilarCha <pchavez92@gmail.com>
2021-03-07 20:50:12 -0800

    Got the right shindig to start

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Timer from '../views/Timer.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Timer',
+    component: Timer
   },
   {
     path: '/about',
---
+++ src/views/Timer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="">
+    Hello it is I the timer
+  </div>
+
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
