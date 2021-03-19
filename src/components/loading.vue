7d513a2c4f5a1cfa9c4dee2f98f4b0275fdeba9f
PilarCha <pchavez92@gmail.com>
2021-03-19 15:16:19 -0700

    added epic spinners

---
+++ src/components/loading.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="fingerprint-spinner">
+    <div class="spinner-ring"></div>
+    <div class="spinner-ring"></div>
+    <div class="spinner-ring"></div>
+    <div class="spinner-ring"></div>
+    <div class="spinner-ring"></div>
+    <div class="spinner-ring"></div>
+    <div class="spinner-ring"></div>
+    <div class="spinner-ring"></div>
+    <div class="spinner-ring"></div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
