7795329a5104c98b9d5e413365a2c0c2001b39cc
PilarCha <pchavez92@gmail.com>
2021-09-17 11:36:26 -0700

    installed mocha with chai. No more sinai

--- src/test/timer-settings.spec.js
+++ src/test/timer-settings.spec.js
@@ -5,6 +5,10 @@ import TimerSettings from '@/components/settingsBar/timer-settings.vue'
 
 describe("TimerSettings", () => {
   it ('should render correctly', () => {
+
     let wrapper = shallowMount(TimerSettings);
+
+    expect(wrapper.find('.mdiClose').text()).to.contain('mdi-close')
+    expect(wrapper.vm.fab).to.be.false
   })
 });
