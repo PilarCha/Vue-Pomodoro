56a8004a780e8510f1457f0a39b0e24f9cafd32c
Pilar Chavez <pchavez92@gmail.com>
2021-12-05 15:48:43 -0800

    Getting started on selecting a User to start the timer for that specific user

--- src/views/UserSelect.vue
+++ src/views/UserSelect.vue
@@ -1,27 +1,44 @@
 <template>
-  <div>
-    {{ JSON.stringify(response, null, 2) }}
-  </div>
+  <v-card dark class="mx-auto" max-width="300" tile>
+    <v-list rounded>
+      <v-subheader>Select User</v-subheader>
+      <v-list-item-group color="primary">
+        <v-list-item v-for="(user, i) in userList" :key="i">
+          <v-list-item-icon>
+            <v-icon>mdi-account</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="user.username"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>mdi-add</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Add user</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-card>
 </template>
 <script>
 import sendAsync from "../query-control/renderer.js";
 export default {
-  data() {
-    return {
-      response: null,
-    };
-  },
-
+  data: () => ({
+    userList: null,
+  }),
   mounted() {
     this.getUsers();
   },
 
   methods: {
     getUsers() {
-      let sql = "Select * from User";
+      let sql = "Select username from User";
       sendAsync(sql).then((result) => {
-        console.log(result);
-        this.response = result;
+        this.userList = result[0];
+        console.log(this.userList);
       });
     },
   },
