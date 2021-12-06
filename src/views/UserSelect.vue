227fcc8d568200bd73e36bf11007c3cd4f39a0f7
Pilar Chavez <pchavez92@gmail.com>
2021-12-05 16:22:48 -0800

    FInetuning the userSelect frontend. Included a globalvariables css file in order to keep all the styles available for all to use!

--- src/views/UserSelect.vue
+++ src/views/UserSelect.vue
@@ -1,22 +1,28 @@
 <template>
-  <v-card dark class="mx-auto" max-width="300" tile>
+  <v-card dark>
     <v-list rounded>
-      <v-subheader>Select User</v-subheader>
+      <v-toolbar style="background-color: var(--settings-pink)" dense>
+        <v-toolbar-title>Select User</v-toolbar-title>
+        <v-spacer></v-spacer>
+
+        <v-btn small class="save-button-color">Quit</v-btn>
+      </v-toolbar>
+      <v-spacer></v-spacer>
       <v-list-item-group color="primary">
-        <v-list-item v-for="(user, i) in userList" :key="i">
+        <v-list-item v-for="(user, i) in userList" :key="i" rounded>
           <v-list-item-icon>
             <v-icon>mdi-account</v-icon>
           </v-list-item-icon>
           <v-list-item-content>
-            <v-list-item-title v-text="user.username"></v-list-item-title>
+            <v-list-item-title v-text="user"></v-list-item-title>
           </v-list-item-content>
         </v-list-item>
         <v-list-item>
           <v-list-item-icon>
-            <v-icon>mdi-add</v-icon>
+            <v-icon>mdi-account-plus</v-icon>
           </v-list-item-icon>
           <v-list-item-content>
-            <v-list-item-title>Add user</v-list-item-title>
+            <v-list-item-title>Add New User</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list-item-group>
@@ -44,4 +50,4 @@ export default {
   },
 };
 </script>
-7
+<style src="@/assets/styles/userSelect.css"></style>
