77b0fa5f67f7c7b78ad4e383a05883a8699d295b
Pilar Chavez <pchavez92@gmail.com>
2021-12-13 14:14:57 -0800

    created the crud ability for the selectedUser.

--- src/store/userStore.js
+++ src/store/userStore.js
@@ -1,8 +1,22 @@
 const user = {
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
+  state: {
+    selectedUser: null,
+  },
+  getters: {
+    selectedUser: (state) => {
+      return state.selectedUser;
+    },
+  },
+  mutations: {
+    UPDATE_SELECTED_USER(state, user) {
+      state.selectedUser = user;
+    },
+  },
+  actions: {
+    setSelectedUser({ commit }, payload) {
+      commit("UPDATE_SELECTED_USER", payload);
+    },
+  },
 };
 
 export default user;
