4f3960478e0831174894b05e0c3b463fb052922f
Pilar Chavez <pchavez92@gmail.com>
2021-12-13 14:12:28 -0800

    created new file to store the user data

--- src/store.js
+++ src/store.js
@@ -1,11 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import timer from "./store/timerStore";
+import user from "./store/userStore";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     timer,
+    user,
   },
 });
