c28ea358819051d2257c3c8b6d806e7643438bf7
Pilar Chavez <pchavez92@gmail.com>
2022-01-05 19:16:33 -0800

    Changed the folder structure to just simple addition to the vue components

--- src/components/settingsBar/timer-settings.vue
+++ src/components/settingsBar/timer-settings.vue
@@ -22,6 +22,8 @@
 
 <script>
 import { mapActions } from "vuex";
+const electron = window.require("electron");
+const { ipcRenderer } = electron;
 export default {
   name: "TimerSettings",
   data: () => ({
@@ -48,6 +50,10 @@ export default {
     },
   },
 };
+
+ipcRenderer.on("displays-available", function (store) {
+  alert(store);
+});
 </script>
 
 <style lang="css" scoped>
