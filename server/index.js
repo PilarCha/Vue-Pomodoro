abff658e14cc37da8eb0278bbdd8a29efc0c5e05
Pilar Chavez <pchavez92@gmail.com>
2022-01-10 16:30:29 -0800

    finished getAllUsers for main page as well as running on port 4000

--- server/index.js
+++ server/index.js
@@ -7,3 +7,20 @@ const server = http.createServer(app);
 const db = new sqlite3.Database("../public/PomodoroTimer.db");
 
 // db.run('CREATE TABLE IF NOT EXISTS')
+app.get("/getAllUsers", (req, res) => {
+  db.serialize(() => {
+    db.all("SELECT id,username FROM Users", (err, rows) => {
+      if (err) {
+        res.send("Error encountered fetching the data");
+        console.error(err.message);
+      }
+      res.json(rows);
+      console.log(rows);
+      console.log("Reached GetAllUsers");
+    });
+  });
+});
+
+server.listen(4000, function () {
+  console.log("Server Listening on port:4000");
+});
