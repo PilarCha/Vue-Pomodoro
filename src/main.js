1c5beb4580a53697428069a0e251d01b4dbf38ee
Pilar Chavez <pchavez92@gmail.com>
2022-01-13 19:10:57 -0800

    Setup axios in main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import vuetify from './plugins/vuetify';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import vuetify from "./plugins/vuetify";
+import axios from "axios";
 
-Vue.config.productionTip = false
+Vue.prototype.$http = axios;
+Vue.config.productionTip = false;
 import store from "./store";
 
 new Vue({
   router,
   vuetify,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
